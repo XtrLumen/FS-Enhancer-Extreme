f14529578b43dd28af341b3b263eb8f116d851d1
XtrLumen <xtrlumen@outlook.com>
2026-07-06 23:40:13 +0800

    Resolve historical issues

--- module/src/script/util_functions.sh
+++ module/src/script/util_functions.sh
@@ -14,27 +14,21 @@
 #
 
 ##VARIABLE##
-#ALIAS#
-TS="tricky_store"
-FSEE="fs_enhancer_extreme"
-S="service.sh"
-D=".fsee_state.sh"
-P="post-fs-data.sh"
 #ZERO LEVEL#
 ADB="/data/adb"
 #ONE LEVEL#
-MODULESDIR="$ADB/modules"
-FSEECONFIG="$ADB/$FSEE"
 SD="$ADB/service.d"
+MODULESDIR="$ADB/modules"
+FSEECONFIG="$ADB/fs_enhancer_extreme"
 #TWO LEVEL#
-FSEEMODDIR="$MODULESDIR/$FSEE"
-TSMODDIR="$MODULESDIR/$TS"
+FSMODDIR="$MODULESDIR/forgestore"
+FSEEMODDIR="$MODULESDIR/fs_enhancer_extreme"
 #THREE LEVEL#
 MULTIPLETYPE="$FSEECONFIG/multiple.txt"
 KERNELTYPE="$FSEECONFIG/kernel.txt"
 FSEELOG="$FSEECONFIG/log/log.log"
-FSEEBIN="$FSEEMODDIR/bin"
 TYPE="$FSEECONFIG/root.txt"
+FSEEBIN="$FSEEMODDIR/bin"
 #OTHER#
 ORIGIN=$(basename "$0")
 ##END##
@@ -45,16 +39,15 @@ ORIGIN=$(basename "$0")
 println() {
   [ "$LOCALE" = "$1" ] && {
     shift
-    if [ "$1" = "-n" ]; then
-      shift
-      echo -n "$@"
-    else
-      echo "$@"
-    fi
+    echo "$@"
   }
 }
-echo_cn() { println "CN" "$@"; }
-echo_en() { println "EN" "$@"; }
+echo_cn() {
+  println "CN" "$@"
+}
+echo_en() {
+  println "EN" "$@"
+}
 #OTHER#
 logout() { echo "$(date "+%m-%d %H:%M:%S.$(date +%3N)")  $$  $$ $1 System.out: [FSEE]$2" >> "$FSEELOG"; }
 logs() { logout "$1" "<Service>$2"; }
@@ -62,13 +55,13 @@ logd() { logout "$1" "<Service.D>$2"; }
 logp() { logout "$1" "<Post-Fs-Data>$2"; }
 invoke() {
   case "$ORIGIN" in
-    *"$S"*)
+    *"service.sh"*)
       class="logs"
       ;;
-    *"$P"*)
+    *"post-fs-data.sh"*)
       class="logp"
       ;;
-    *"$D"*)
+    *".fsee_state.sh"*)
       class="logd"
       ;;
   esac
@@ -80,19 +73,19 @@ invoke() {
   fi
 }
 check() {
-  if [ "$(cat "$TYPE")" = "Multiple" ] || [ ! -d "$TSMODDIR" ] || [ -f "$TSMODDIR/disable" ] || sed -n '5p' "$TSMODDIR/module.prop" | grep -q -F "Enginex0"; then
+  if [ "$(cat "$TYPE")" = "Multiple" ] || [ ! -d "$FSMODDIR" ] || [ -f "$FSMODDIR/disable" ] || sed -n '5p' "$FSMODDIR/module.prop" | grep -q -F "Enginex0"; then
     case "$ORIGIN" in
-      *"$P"*)
+      *"post-fs-data.sh"*)
         logp "E" "环境异常,拦截执行"
         mv "$FSEEMODDIR/webroot" "$FSEEMODDIR/.webroot"
         mv "$FSEEMODDIR/action.sh" "$FSEEMODDIR/.action.sh"
         ;;
-      *"$S"*)
+      *"service.sh"*)
         exit
         ;;
     esac
   else
-    [[ "$ORIGIN" == *"$P"* ]] && {
+    [[ "$ORIGIN" == *"post-fs-data.sh"* ]] && {
       logp "I" "环境正常,继续执行"
       mv "$FSEEMODDIR/.webroot" "$FSEEMODDIR/webroot"
       if [[ ! "$APATCH" && ! "$KSU" ]]; then
@@ -103,15 +96,6 @@ check() {
     }
   fi
 }
-detect() {
-  if [ $? -eq 0 ]; then
-    echo_cn "完毕"
-    echo_en "Complete"
-  else
-    echo_cn "失败"
-    echo_en "Failed"
-  fi
-}
 initwait() {
   until [ $(getprop sys.boot_completed) -eq 1 ]; do
     sleep 1s
@@ -119,7 +103,7 @@ initwait() {
 }
 ##END##
 
-[[ "$ORIGIN" == *"$P"* ]] && {
+[[ "$ORIGIN" == *"post-fs-data.sh"* ]] && {
   rm -f "$MULTIPLETYPE"
   rm -f "$KERNELTYPE"
   rm -f "$FSEELOG"
